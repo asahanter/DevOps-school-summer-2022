7894daa12aab49f7045eb16ede1a4df1dc6f6f00
Alexander Sahan <mortar5900@gmail.com>
2022-07-14 23:13:01 +0300

    Remove files

--- Bash_task1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir Sahan
-cd Sahan
-mkdir -p EEE/GR1 FFF JJJ/GR2
-echo "SummerSchool 2022 DevOps" > EEE/GR1/Alexander.txt
-date >> EEE/GR1/Alexander.txt
-echo "oleksandr.sahan95@gmail.com" > JJJ/GR2/Sahan.txt
--- Bash_task2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd Sahan/EEE/GR1/
-cp Alexander.txt ../../Alexander.txt
-cd ../..
-cd JJJ/GR2/
-cp Sahan.txt ../../Sahan.txt
-
